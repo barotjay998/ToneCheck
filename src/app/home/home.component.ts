3dc27a3741831e980fdadbaaa89e0d31d809a768
jay <barotjay998>
2024-01-23 14:53:36 -0600

    service remove

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,30 +11,13 @@ export class HomeComponent {
 
   constructor(
     private router: Router,
-    private customerService: ClickworkerTcApiService
   ) { }
 
   ngOnInit() {
-    this.checkCustomer(1234);
-    this.addCustomer(1234);
   }
 
   onInternalLink(linkUri: string) {
     this.router.navigate([linkUri]);
-  }
-
-  checkCustomer(customerId: number) {
-    this.customerService.checkCustomer(customerId).subscribe({
-      next: (response) => console.log(response.message),
-      error: (error) => console.error(error.error.message)
-    });
-  }
-
-  addCustomer(customerId: number) {
-    this.customerService.addCustomer(customerId).subscribe({
-      next: (response) => console.log(response.message),
-      error: (error) => console.error(error.error.message)
-    });
   }
 
 }
